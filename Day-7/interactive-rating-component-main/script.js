cffefc9657f7776e93ac812f2e88ae1b6117dadf
cyrus303 <sachinsunny303@gmail.com>
2022-07-08 07:27:39 +0100

    rating card done

--- Day-7/interactive-rating-component-main/script.js
+++ Day-7/interactive-rating-component-main/script.js
@@ -1,12 +1,13 @@
 const numbers = document.querySelectorAll('.number');
 const submitBtn = document.querySelector('.submit');
+const screenOne = document.querySelector('.screen-1');
+const screenTwo = document.querySelector('.screen-2');
 
-numbers.forEach((number) => {
-  number.addEventListener('click', (e) => {
-    revertStyle();
-    number.classList.toggle('highlight');
-  });
-});
+const mainContainer = document.querySelector('.main-container');
+
+// Main Screen
+
+let rating = 0;
 
 function revertStyle() {
   numbers.forEach((number) => {
@@ -14,6 +15,34 @@ function revertStyle() {
   });
 }
 
+function showScreen2() {
+  screenTwo.innerHTML = `
+  <img src="./images/illustration-thank-you.svg" alt="" />
+
+  <div class="final-rating">
+    <div class="rating">You selected ${rating} out of 5</div>
+  </div>
+  <h1>Thank you!</h1>
+  <p>
+    We appreciate you taking the time to give a rating. if you ever need
+    more support, dont hesitate to get in touch
+  </p>
+  `;
+  mainContainer.append(screenTwo);
+}
+
+numbers.forEach((number) => {
+  number.addEventListener('click', (e) => {
+    revertStyle();
+    number.classList.add('highlight');
+    rating = number.innerHTML;
+  });
+});
+
 submitBtn.addEventListener('click', (e) => {
   submitBtn.classList.toggle('clicked');
+  screenOne.classList.add('first-page-display');
+  showScreen2();
 });
+
+// thank you screen
