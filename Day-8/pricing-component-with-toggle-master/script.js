1d8992e99c9102ead8155eb8fc585834671add6e
cyrus303 <sachinsunny303@gmail.com>
2022-07-09 10:18:09 +0100

    challenge done

--- Day-8/pricing-component-with-toggle-master/script.js
+++ Day-8/pricing-component-with-toggle-master/script.js
@@ -1,12 +1,19 @@
 const toggleContainer = document.querySelector('.toggle-bg');
 const toggleBtn = document.querySelector('.toggle-button');
 
-const price = document.querySelector('#price');
+const value1 = document.querySelector('.value1');
+const value2 = document.querySelector('.value2');
+const value3 = document.querySelector('.value3');
 
 toggleContainer.addEventListener('click', (e) => {
   toggleBtn.classList.toggle('switch');
-
   if (toggleBtn.classList.contains('switch')) {
-    console.log(price.innerHTML);
+    value1.innerHTML = 15.99;
+    value2.innerHTML = 24.99;
+    value3.innerHTML = 34.99;
+  } else {
+    value1.innerHTML = 199.99;
+    value2.innerHTML = 249.99;
+    value3.innerHTML = 399.99;
   }
 });
