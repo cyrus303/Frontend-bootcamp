13203e37480e867b2fef3e5e647e4574eb0c7fff
cyrus303 <sachinsunny303@gmail.com>
2022-07-23 08:17:13 +0100

    deployed onine

--- Day-20/meme-practice/src/components/Form.js
+++ Day-20/meme-practice/src/components/Form.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../index.css';
-import data from '../memeData.js';
+// import data from '../memeData.js';
 
 function Form() {
-  //   console.log(memeData);
-
-  const [memeData] = useState(data);
+  const [memeData, setMemeData] = useState([]);
   const [meme, setMeme] = useState({
     topText: '',
     bottomText: '',
     memeURL: 'https://i.imgflip.com/28j0te.jpg',
   });
 
+  const apiURL = 'https://api.imgflip.com/get_memes';
+
+  useEffect(() => {
+    console.log('useEffect ran');
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((result) => setMemeData(result.data.memes));
+  }, []);
+
+  //   console.log(memeData);
+
   function getRandomMeme() {
     let randomNumber = Math.floor(Math.random() * 100);
-    let randomMeme = memeData.data.memes[randomNumber];
+    let randomMeme = memeData[randomNumber];
     let randomMemeURL = randomMeme.url;
     setMeme((prevData) => {
       return {
@@ -34,8 +43,6 @@ function Form() {
     });
     // console.log(value, name);
   }
-
-  console.log(meme);
 
   return (
     <section className="form-container">
