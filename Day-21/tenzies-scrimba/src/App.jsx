722053d8b8522fd3fbf9c2cb4142bba68a8969b5
cyrus303 <sachinsunny303@gmail.com>
2022-07-25 23:10:40 +0100

    added toggle

--- Day-21/tenzies-scrimba/src/App.jsx
+++ Day-21/tenzies-scrimba/src/App.jsx
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
 import './App.css';
 import Die from './components/die';
+import { nanoid } from 'nanoid';
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
 
   const diceElements = dice.map((die) => {
-    return <Die value={die} />;
+    return (
+      <Die
+        key={die.id}
+        value={die.value}
+        isHeld={die.isHeld}
+        id={die.id}
+        holdDice={holdDice}
+      />
+    );
   });
 
   function allNewDice() {
-    let newDice = [];
+    const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
     }
     return newDice;
   }
 
   function rollDice() {
     setDice(allNewDice());
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : { ...die };
+      })
+    );
   }
 
   return (
--- Day-21/tenzies-scrimba/src/components/die.jsx
+++ Day-21/tenzies-scrimba/src/components/die.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import './die.css';
 
 function Die(props) {
-  const { value } = props;
+  const { value, isHeld, id, holdDice } = props;
+
+  const styles = {
+    backgroundColor: isHeld ? '#59E391' : 'white',
+  };
+
+  function displayId() {
+    holdDice(id);
+  }
+
   return (
-    <div className="die-container">
+    <div className="die-container" style={styles} onClick={displayId}>
       <p>{value}</p>
     </div>
   );
